3be436581f9b8d62e44f4e0ea3b462fed20ed9a0
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-04-03 11:06:26 +0200

    add xz, change path to updated config files

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build-aux
 
 set -e -o pipefail -x
 
