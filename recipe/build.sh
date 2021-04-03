8ac6bd68411000de34dd08488e5be142b97e6f70
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-04-03 20:17:33 +0200

    ..

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ set -e -o pipefail -x
 if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 && $target_platform == osx-arm64 ]]; then
 	# Get an updated config.sub and config.guess
 	cp -v $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
-	# cp src/syscfg/lock-obj-pub.aarch64-apple-darwin.h src/syscfg/lock-obj-pub.darwin20.0.0.h
+	cp src/syscfg/lock-obj-pub.aarch64-apple-darwin.h src/syscfg/lock-obj-pub.darwin20.0.0.h
 fi
 
 autoreconf -if
